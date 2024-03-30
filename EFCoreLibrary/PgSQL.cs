4290167e716c35029a369250f22866402fcf7c28
Max Kornilev <kornilev.maxim@mail.ru>
2024-03-30 21:18:08 +0300

    v 2.2

---
+++ EFCoreLibrary/PgSQL.cs
@@ -0,0 +1,14 @@
+﻿/*
+ * 1) NuGet -> Npgsql.EntityFrameworkCore.PostgreSQL
+ * 2) connectionString -> 
+ *  {
+     "SqlConnectionString": "Data Source=MSI\\SQLEXPRESS;Database=MSTUCABotDb;Integrated Security=true;TrustServerCertificate=True; Encrypt=False;",
+     "PostgreeSqlConnectionString": "Server=localhost;Database=MSTUCABotDb;Port=5432;User Id=postgres;Password=MaksPlay2;"
+	}
+
+	3) Method change:
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseNpgsql(_connectionString);
+		}
+ */
